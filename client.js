ad5940bebbcdf95cd913b6ad1620b95b9996b94a
jgoncalvesjr <jgoncalvesjr@gmail.com>
2020-07-29 18:31:54 +0000

    Snake game - multiplayer messages

--- client.js
+++ client.js
@@ -4,30 +4,13 @@ const net = require('net');
  * Establishes connection with the game server
  */
 
-
-
-const connect = function() {
+ const connect = function() {
   let delay = 0;
   const conn = net.createConnection({ 
     host: '135.23.222.131 ',
     port: 50541
   });
 
-  const up = () => {
-    conn.write('Move: up');
-  }
-  
-  const down = () => {
-    conn.write('Move: down');
-  }
-  
-  const left = () => {
-    conn.write('Move: left');
-  }
-  
-  const right = () => {
-    conn.write('Move: right');
-  }
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
@@ -42,16 +25,6 @@ const connect = function() {
   console.log('Connection estabilished!');
   const clientName = 'JÃO';
   conn.write(`Name: ${clientName}`);
-  // setTimeout(() => up(), 250);
-  // setTimeout(() => up(), 500);
-  // setTimeout(() => up(), 750);
-  // setTimeout(() => up(), 1000);
-  // setTimeout(() => left(), 1250);
-  // setTimeout(() => left(), 1500);
-  // setTimeout(() => left(), 1750);
-  // setTimeout(() => left(), 2000);
-  // setInterval(() => down(), 100);
-
   });
   
 
--- input.js
+++ input.js
@@ -5,17 +5,26 @@ const handleUserInput = key => {
     case '\u0003': // ctrl+c to bail from game
     process.exit();
     break;
-    case '\u0077':
+    case '\u0077': // w
     connection.write('Move: up');
     break;
-    case '\u0061':
+    case '\u0061': // a
     connection.write('Move: left');
     break;
-    case '\u0073':
+    case '\u0073': // s
     connection.write('Move: down');
     break;
-    case '\u0064':
+    case '\u0064': // d
     connection.write('Move: right');
+    break;
+    case '\u006A': // j
+    connection.write('Say: Sup n00b');
+    break;
+    case '\u006B': // k 
+    connection.write('Say: Gotcha STONKS!');
+    break;
+    case '\u006C': // l
+    connection.write('Say: BASED move');
     break;
   }
 }
