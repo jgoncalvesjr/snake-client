6d9017b7383d0bfc9a1cb4e3b5fc8aba242e8448
jgoncalvesjr <jgoncalvesjr@gmail.com>
2020-07-29 17:37:11 +0000

    Snake game - movement part 1

--- client.js
+++ client.js
@@ -3,11 +3,31 @@ const net = require('net');
 /**
  * Establishes connection with the game server
  */
+
+
+
 const connect = function() {
+  let delay = 0;
   const conn = net.createConnection({ 
     host: '135.23.222.131 ',
     port: 50541
   });
+
+  const up = () => {
+    conn.write('Move: up');
+  }
+  
+  const down = () => {
+    conn.write('Move: down');
+  }
+  
+  const left = () => {
+    conn.write('Move: left');
+  }
+  
+  const right = () => {
+    conn.write('Move: right');
+  }
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
@@ -16,10 +36,22 @@ const connect = function() {
   console.log(message);
   });
 
+  // event handler for confirming connection and creating player name
+
   conn.on('connect', function () {
   console.log('Connection estabilished!');
   const clientName = 'JÃO';
   conn.write(`Name: ${clientName}`);
+  // setTimeout(() => up(), 250);
+  // setTimeout(() => up(), 500);
+  // setTimeout(() => up(), 750);
+  // setTimeout(() => up(), 1000);
+  // setTimeout(() => left(), 1250);
+  // setTimeout(() => left(), 1500);
+  // setTimeout(() => left(), 1750);
+  // setTimeout(() => left(), 2000);
+  // setInterval(() => down(), 100);
+
   });
   
 
