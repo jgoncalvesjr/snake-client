713e57b8e8679c282ab58e9102425386f92692c3
jgoncalvesjr <jgoncalvesjr@gmail.com>
2020-07-29 17:05:54 +0000

    Snake game - client setup and obj shorthanding

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '135.23.222.131 ',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  // event handle for receiving incoming message from server
+  conn.on('data', function (message) {
+  console.log(message);
+  });
+
+  return conn;
+}
+
+module.exports = {
+  connect
+}
