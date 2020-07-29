f6f9b63535a0f4cfc33ff19a7a3f5382a153cdd5
jgoncalvesjr <jgoncalvesjr@gmail.com>
2020-07-29 18:09:49 +0000

    Snake game - playable keyboard

--- input.js
+++ input.js
@@ -1,11 +1,27 @@
+let connection;
+
 const handleUserInput = key => {
   switch (key) {
     case '\u0003': // ctrl+c to bail from game
     process.exit();
+    break;
+    case '\u0077':
+    connection.write('Move: up');
+    break;
+    case '\u0061':
+    connection.write('Move: left');
+    break;
+    case '\u0073':
+    connection.write('Move: down');
+    break;
+    case '\u0064':
+    connection.write('Move: right');
+    break;
   }
 }
 
 const setupInput = conn => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
